99da50c04aeecd8f6759724085641f37d169ef0b
Joel Flores <joel.flores3000@protonmail.com>
2023-03-08 17:44:50 -0400

    ejercicio 3 completo

--- 5.Ejercicios_de_diccionarios/Ejercicio_3.py
+++ 5.Ejercicios_de_diccionarios/Ejercicio_3.py
@@ -1,1 +1,18 @@
-'''Ejercicio 3.'''
+'''Ejercicio 3.
+Escribir un programa que guarde en un diccionario los precios de las frutas de la tabla, pregunte al usuario por una fruta, un número de kilos y muestre por pantalla el precio de ese número de kilos de fruta. Si la fruta no está en el diccionario debe mostrar un mensaje informando de ello.'''
+
+def run():
+    frutas = {'Platano' : 1.35, 'Manzana': 0.80, 'Pera': 0.85, 'Naranja' : 0.70}
+    try:
+        fruta = str(input('que fruta queres saber su precio: ')).capitalize()
+        if not fruta in frutas:
+            print(f'Lo siento no tengo el precio de la fruta {fruta}.')
+        else:
+            cantidad = float(input('cuantos kilos son: '))
+            precio = frutas[fruta]*cantidad
+            print(f'{cantidad}Kg. de {fruta} tiene un precio de: {round(precio, 2)}')
+    except:
+        print('Lo siento los datos que me pides son incorrectos.')
+
+if __name__ == '__main__':
+    run()
